f287dee5c2526a5e1ebaca9554c298d1bf14ec07
mustafah15 <mustafa.hussain93@gmail.com>
2020-12-24 15:17:12 +0200

    allow cors

--- auction-service/src/lib/commonMiddleware.js
+++ auction-service/src/lib/commonMiddleware.js
@@ -2,10 +2,12 @@ import middy from "@middy/core";
 import httpJsonBodyParser from "@middy/http-json-body-parser";
 import httpErrorHandler from "@middy/http-error-handler";
 import httpEventNormalizer from "@middy/http-event-normalizer";
+import cors from '@middy/http-cors';
 
 export default handler => middy(handler)
     .use([
         httpJsonBodyParser(),
         httpErrorHandler(),
         httpEventNormalizer(),
+        cors(),
     ]);
